b3e557d274c4f36ac7de36580f412349be22c678
Remo Pini <remo.pini@avexys.com>
2022-10-01 12:07:24 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WebViewTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace WebViewTest;
+
+public partial class MainPage : ContentPage
+{
+    HtmlWebViewSource src = new();
+
+    public MainPage()
+    {
+        InitializeComponent();
+        web.Source = src;
+    }
+
+    private void btn_Clicked(object sender, EventArgs e)
+    {
+        Random rnd = new();
+        src.Html = $"<h1>Hello World {rnd.Next()}!</h1><br>";
+        src.Html = $"<h1>Hello World {rnd.Next()}!</h1>";
+        web.Reload();
+    }
+}
