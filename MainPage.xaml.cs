98a6595a63fc2c98922ceb5723d22302208b2e79
Remo Pini <remo.pini@avexys.com>
2022-10-01 12:11:44 +0200

    Added comment

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,8 +13,8 @@ public partial class MainPage : ContentPage
     private void btn_Clicked(object sender, EventArgs e)
     {
         Random rnd = new();
-        src.Html = $"<h1>Hello World {rnd.Next()}!</h1><br>";
-        src.Html = $"<h1>Hello World {rnd.Next()}!</h1>";
+        src.Html += $"<h1>Hello World {rnd.Next()}!</h1><br>";
+        //src.Html += $"<h1>Hello World {rnd.Next()}!</h1><br>"; //uncommenting this willl crash the app with a non handled win32 exception...
         web.Reload();
     }
 }
